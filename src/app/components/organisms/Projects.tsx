5449860f0468516698edf50613e7c1374086222b
Edward Daniel Reyes Pérez <edwardreyessp@gmail.com>
2024-03-21 02:39:05 -0600

    feat(organism): Infinite project slider without fade

--- src/app/components/organisms/Projects.tsx
+++ src/app/components/organisms/Projects.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { Box, type SxProps, Typography, useTheme } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { ProjectCard } from '../molecules';
 import styles from './Projects.module.scss';
 import AppPerro from '../../../../public/images/AppPerro.png';
@@ -9,17 +8,6 @@ import DinoGame from '../../../../public/images/DinoGame.png';
 import Fiorel from '../../../../public/images/Fiorel.png';
 
 export const Projects = () => {
-	const isDark = useTheme().palette.mode === 'dark';
-
-	const styleSlider: SxProps = {
-		'&::before, &::after': {
-			width: { xs: '15%', md: '20%' },
-			background: isDark
-				? 'linear-gradient(to right, #121212 0%, rgba(18, 18, 18, 0) 100%)'
-				: 'linear-gradient(to right, white 0%, rgba(255, 255, 255, 0) 100%)',
-		},
-	};
-
 	return (
 		<Box>
 			<Box paddingX={{ xs: 5, md: 15 }}>
@@ -30,7 +18,7 @@ export const Projects = () => {
 					Conoce mi mundo
 				</Typography>
 			</Box>
-			<Box sx={styleSlider} className={styles.slider}>
+			<Box className={styles.slider}>
 				<div className={styles['slide-track']}>
 					<div className={styles.slide}>
 						<ProjectCard
